2db5addba240fe6e6f15d8ec308a121efe5c210e
apple01644 <banana2692@daum.net>
2020-10-08 14:30:35 +0900

    First Commit

---
+++ controller.py
@@ -0,0 +1,51 @@
+import os, time
+from ctypes import CFUNCTYPE, c_char_p, c_int, cdll
+
+if os.name == 'posix':
+    import RPi.GPIO as gpio
+
+
+    class Controller:
+        button_pins = [22, 33, 16, 18, ]
+
+        def __init__(self, mp3_player):
+            error_handler_template = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+            def py_error_handler(filename, line, function, err, fmt):
+                pass
+
+            c_error_handler = error_handler_template(py_error_handler)
+            self.lib_sound = cdll.LoadLibrary('libasound.so')
+            self.lib_sound.snd_lib_error_set_handler(c_error_handler)
+            self.mp3_player = mp3_player
+
+            gpio.setmode(gpio.BOARD)
+            gpio.setwarnings(False)a
+
+            def on_click_prev(ch):
+                mp3_player.prev_song()
+
+            def on_click_next(ch):
+                mp3_player.next_song()
+
+            def on_click_big_button(ch):
+                mp3_player.big_button()
+
+            for button_pin in self.button_pins:
+                gpio.setup(button_pin, gpio.IN)
+            gpio.add_event_detect(self.button_pins[0], gpio.FALLING, callback=on_click_prev, bouncetime=600)
+            gpio.add_event_detect(self.button_pins[1], gpio.FALLING, callback=on_click_next, bouncetime=600)
+            #gpio.add_event_detect(self.button_pins[4], gpio.FALLING, callback=on_click_big_button, bouncetime=600)
+
+        def loop(self):
+            if gpio.input(self.button_pins[2]) == gpio.HIGH:
+                self.mp3_player.volume_down()
+            if gpio.input(self.button_pins[3]) == gpio.HIGH:
+                self.mp3_player.volume_up()
+            time.sleep(0.01)
+
+
+else:
+    class Controller:
+        pass
+        # May use only RaspberryPi
---
+++ mp3_player.py
@@ -0,0 +1,112 @@
+#!python3
+
+import os
+import sys
+import time
+
+import pygame
+
+from controller import Controller
+
+
+class MP3Player:
+    RUN_DIR = '/home/pi/Desktop/example/'
+    MUSIC_DIR = '/home/pi/Desktop/example/album/'
+    music_list = []
+    music_volume = 0.5
+    music_index = 0
+    controller = None
+    music_paused = False
+
+    def __init__(self):
+        print('[TRY] file loaded')
+        while True:
+            if os.path.isdir(self.MUSIC_DIR):
+                for f in os.listdir(self.MUSIC_DIR):
+                    if f.find('.mp3') != -1:
+                        self.music_list.append(f)
+            if len(self.music_list) > 0:
+                break
+            else:
+                time.sleep(0.1)
+        print('[SUCC] file loaded')
+
+        print('[TRY] Load music index')
+        try:
+            with open('music_index.dat', 'r') as f:
+                data = f.read()
+                if data:
+                    self.music_index = int(data)
+                    if self.music_index >= len(self.music_list):
+                        self.music_index = 0
+            print('[SUCC] Load music index')
+        except:
+            print('[FAIL] Load music index')
+
+    def volume_up(self):
+        self.music_volume += 0.01
+        if self.music_volume > 1:
+            self.music_volume = 1
+        try:
+            pygame.mixer.music.set_volume(self.music_volume)
+        except:
+            pass
+
+    def volume_down(self):
+        self.music_volume -= 0.01
+
+        if self.music_volume < 0:
+            self.music_volume = 0
+        try:
+            pygame.mixer.music.set_volume(self.music_volume)
+        except:
+            pass
+
+    def prev_song(self):
+        self.music_index = (self.music_index - 1 + len(self.music_list)) % len(self.music_list)
+        path = self.MUSIC_DIR + self.music_list[self.music_index]
+        print(self.music_list[self.music_index], path)
+        pygame.mixer.music.load(path)
+        pygame.mixer.music.set_volume(self.music_volume)
+        pygame.mixer.music.play()
+        self.music_paused = False
+        with open('music_index.dat', 'w') as f:
+            f.write(str(self.music_index))
+
+    def next_song(self):
+        self.music_index = (self.music_index + 1) % len(self.music_list)
+        path = self.MUSIC_DIR + self.music_list[self.music_index]
+        print(self.music_list[self.music_index], path)
+        pygame.mixer.music.load(path)
+        pygame.mixer.music.set_volume(self.music_volume)
+        pygame.mixer.music.play()
+        self.music_paused = False
+        with open('music_index.dat', 'w') as f:
+            f.write(str(self.music_index))
+
+    def big_button(self):
+        if self.music_paused:
+            pygame.mixer.music.unpause()
+        else:
+            pygame.mixer.music.pause()
+        self.music_paused = not self.music_paused
+
+    def main_loop(self):
+        pygame.init()
+        pygame.mixer.init()
+
+        pygame.mixer.music.load(self.RUN_DIR + '/start.mp3')
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play()
+
+        self.controller = Controller(self)
+        while True:
+            self.controller.loop()
+        pygame.mixer.quit()
+        pygame.quit()
+
+
+mp3_player = MP3Player()
+
+if __name__ == '__main__':
+    mp3_player.main_loop()
