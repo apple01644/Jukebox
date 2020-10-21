25fb50cb9e4ede86da74709756fd64a5119e38b8
yun_j_sang(윤재상) <apple01644@gmail.com>
2020-10-21 15:32:42 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ cp error.mp3 /etc/jukebox/error.mp3
 cp music_loader.py /etc/jukebox/music_loader.py
 cp controller.py /etc/jukebox/controller.py
 cp jukebox.py /etc/jukebox/jukebox.py
+sudo touch /etc/jukebox/music_index.dat
+sudo chmod u=rw /etc/jukebox/music_index.dat
+sudo chown pi /etc/jukebox/music_index.dat
+
 sudo cp jukebox.service /etc/systemd/system/jukebox.service
 sudo systemctl enable jukebox
 sudo systemctl start  jukebox
