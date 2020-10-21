0e21c026ef886f133c3c417294dde4bb2d785f58
123444 <123123>
2020-10-21 15:08:59 +0900

    Fix music_loader.py

--- music_loader.py
+++ music_loader.py
@@ -9,10 +9,11 @@ def get_music_list(base_dir):
     def _find_music(music_dir):
         for file_name in os.listdir(music_dir):
             abs_path = music_dir + file_name
-            if abs_path.find('.mp3') != -1:
+            if os.path.isfile(abs_path) and abs_path.find('.mp3') != -1:
                 print('Add music', abs_path)
                 music_list.append(abs_path)
             elif os.path.isdir(abs_path):
+                abs_path += '/'
                 print('Explore', abs_path)
                 _find_music(abs_path)
             else:
