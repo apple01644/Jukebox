050930eb669635b4bf94f06fe8a0afa1b1029830
yun_j_sang(윤재상) <apple01644@gmail.com>
2020-10-21 13:26:57 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ sudo rm -rf /etc/jukebox
 sudo mkdir /etc/jukebox
 
 cp start.mp3 /etc/jukebox/start.mp3
-cp start.mp3 /var/music/start.mp3
 cp controller.py /etc/jukebox/controller.py
 cp jukebox.py /etc/jukebox/jukebox.py
 sudo cp jukebox.service /etc/systemd/system/jukebox.service
